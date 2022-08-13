316c73c25811cc5475b0ef52d8113f8ef7d62f80
Andrey <81762984+User6531@users.noreply.github.com>
2022-08-13 21:44:34 +0300

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,37 +1,47 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, {
+  AxiosError,
+  AxiosRequestConfig,
+  AxiosRequestHeaders,
+} from "axios";
 import { refreshAccessToken } from "./refreshAccessToken";
+
+const getRequestHeaders = (accessToken: string): AxiosRequestHeaders => ({
+  accept: "text/plain",
+  Authorization: accessToken,
+});
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    accept: "text/plain",
-  },
+  headers: getRequestHeaders(""),
 });
 
 instance.interceptors.request.use(
-  async (config: AxiosRequestConfig) => {
-    if (config.headers === undefined) {
-      config.headers = {};
-    }
-
-    const accessToken = localStorage.getItem("accessToken");
-    config.headers.Authorization = accessToken || "";
-
-    return config;
-  },
+  async (config: AxiosRequestConfig) => ({
+    ...config,
+    headers: getRequestHeaders(localStorage.getItem("accessToken") || ""),
+  }),
   (error) => error
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status === 401) {
+      return unauthorizedHandling(error);
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
 
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response.status === 401) {
-      (async () => refreshAccessToken())();
+const unauthorizedHandling = async (error: AxiosError) =>
+  instance.request({
+    ...error.config,
+    headers: getRequestHeaders((await refreshAccessToken()) || ""),
+  });
 
-      return instance.request(error.config);
-    }
-    return error;
-  }
-);
+// msalInstance.logoutRedirect({
+//   // Return false if you would like to stop navigation after local logout
+//   onRedirectNavigate: () => false,
+// });
