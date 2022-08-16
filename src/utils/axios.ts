0b3845893217cd2b426543d947124e7f71224961
Andrey <81762984+User6531@users.noreply.github.com>
2022-08-16 10:22:46 +0300

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,47 +1,52 @@
-import axios, {
-  AxiosError,
-  AxiosRequestConfig,
-  AxiosRequestHeaders,
-} from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { msalInstance } from "..";
 import { refreshAccessToken } from "./refreshAccessToken";
-
-const getRequestHeaders = (accessToken: string): AxiosRequestHeaders => ({
-  accept: "text/plain",
-  Authorization: accessToken,
-});
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: getRequestHeaders(""),
+  headers: {
+    accept: "text/plain",
+  },
 });
 
+// Request interceptor for API calls
 instance.interceptors.request.use(
   async (config: AxiosRequestConfig) => ({
     ...config,
-    headers: getRequestHeaders(localStorage.getItem("accessToken") || ""),
+    headers: {
+      ...config.headers,
+      Authorization: localStorage.getItem("accessToken") || "",
+    },
   }),
-  (error) => error
+  (error) => {
+    Promise.reject(error);
+  }
 );
 
+// Response interceptor for API calls
 instance.interceptors.response.use(
-  (response) => response,
+  (response: AxiosResponse) => response,
   async (error) => {
-    if (error.response.status === 401) {
-      return unauthorizedHandling(error);
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401) {
+      if (!originalRequest._retry) {
+        originalRequest._retry = true;
+        const accessToken = (await refreshAccessToken()) || "";
+        axios.defaults.headers.common["Authorization"] = accessToken;
+
+        return instance(originalRequest);
+      } else {
+        msalInstance.logoutRedirect({
+          // Return false if you would like to stop navigation after local logout
+          onRedirectNavigate: () => false,
+        });
+
+        return;
+      }
     }
     return Promise.reject(error);
   }
 );
 
 export default instance;
-
-const unauthorizedHandling = async (error: AxiosError) =>
-  instance.request({
-    ...error.config,
-    headers: getRequestHeaders((await refreshAccessToken()) || ""),
-  });
-
-// msalInstance.logoutRedirect({
-//   // Return false if you would like to stop navigation after local logout
-//   onRedirectNavigate: () => false,
-// });
